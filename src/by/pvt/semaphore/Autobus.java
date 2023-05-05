e10fd0f04d6f4e80133b5a7423e2bd3981b1df22
artem <artem.yurev.1997@gmail.com>
2023-05-06 00:28:53 +0300

    task

--- src/by/pvt/semaphore/Autobus.java
+++ src/by/pvt/semaphore/Autobus.java
@@ -2,26 +2,6 @@ package by.pvt.semaphore;
 
 import java.util.concurrent.Semaphore;
 
-public class Autobus implements Runnable {
-    private String name;
-    private Semaphore semaphore;
+public class Autobus {
 
-    public Autobus(String name, Semaphore semaphore) {
-        this.name = name;
-        this.semaphore = semaphore;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("Автобус " + name + " останавливается на остановке \"Площадь Ленина\"");
-        try {
-            semaphore.acquire();
-            for(int i = 0; i < 10; i++) {
-                System.out.println("автобус едет " + i + " раз");
-            }
-            semaphore.release();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
--- src/by/pvt/semaphore/Main.java
+++ src/by/pvt/semaphore/Main.java
@@ -3,14 +3,5 @@ package by.pvt.semaphore;
 import java.util.concurrent.Semaphore;
 
 public class Main {
-    public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(1);
-        Autobus autobus1 = new Autobus("BMW", semaphore);
-        Autobus autobus2 = new Autobus("TRIO", semaphore);
-        Autobus autobus3 = new Autobus("RDT", semaphore);
 
-        autobus1.run();
-        autobus2.run();
-        autobus3.run();
-    }
 }
